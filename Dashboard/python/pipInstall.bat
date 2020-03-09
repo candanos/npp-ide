b7ce2f467f311895e92d5a9995a8dfc6fca9ac2b
candanos <index@candanos>
2020-03-09 10:27:59 +0300

    yac from lenovo

---
+++ Dashboard/python/pipInstall.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM pip uninstall JPype1
+pip install jaydebeapi
+REM pip install mysql-connector-python
+REM pip install --upgrade pip
+REM pip install cx_oracle
+REM pip install ibm-db
+REM pip install pyinstaller
+REM pip install mysql-connector-python 
+REM pip install flask-bootstrap
+REM pip install flask-wtf
+REM pip install virtualenv
+REM pip install flask
+REM pip install openpyxl
+REM pip install beautifulsoup4
+REM pip install requests
+
+REM uninstalling:
+REM pip uninstall JPype1
+REM pip uninstall jaydebeapi
+
+REM installing a specific version
+REM pip install JPype1==0.6.3
