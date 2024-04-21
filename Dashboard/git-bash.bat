46806f056550618d525740361e1c46c8b4032e77
Yuksel Candan (Consultant) <candan.yuksel@consultant.volvo.com>
2024-04-21 13:12:56 +0200

    something changed.

--- Dashboard/git-bash.bat
+++
@@ -1,6 +0,0 @@
-@REM To run with: "C:\npp-ide\Dashboard\exec-this.bat" "$(CURRENT_DIRECTORY)" $(FILE_NAME)
-cd %1
-Call "C:\Program Files\Git\git-bash.exe"
-pause
-read -p "Press enter to continue"
-exit
